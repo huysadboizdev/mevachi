0f3e65ab96b72244daaa4aa8da47ad264d80c3ff
huysadboizdev <haquanghuydz1996@gmail.com>
2025-03-08 07:34:47 +0700

    me_va_chi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẹ và Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Mẹ Và Chị';
+const contentCard = 'Gửi Mẹ và Chị yêu quý, nhân ngày 8/3,em chúc Mẹ và Chị luôn mạnh khỏe, hạnh phúc và tràn đầy niềm vui. Cảm ơn Mẹ và Chị đã luôn yêu thương, quan tâm và là chỗ dựa vững chắc cho em.! ❤️🌸';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'me.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
